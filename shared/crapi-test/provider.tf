e6c065a33e5a431b5096194154a02882da4c1ab3
Shubham Mishra <90624610+shubhammishra9@users.noreply.github.com>
2024-07-01 16:04:42 +0530

    test-run

--- shared/crapi-test/provider.tf
+++ shared/crapi-test/provider.tf
@@ -2,6 +2,13 @@ provider "aws" {
     region     = local.aws_region
 }
 
+provider "kubectl" {
+    host = local.host
+    cluster_ca_certificate = base64decode(local.cluster_ca_certificate)
+    token = data.aws_eks_cluster_auth.auth.token
+    load_config_file       = false
+}
+
 provider "helm" {
   kubernetes {
     host                   = local.host
