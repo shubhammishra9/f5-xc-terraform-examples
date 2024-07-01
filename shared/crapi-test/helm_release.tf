82279a353f94a72b0ae4a2568b12614bb3006e65
Shubham Mishra <90624610+shubhammishra9@users.noreply.github.com>
2024-07-01 19:07:13 +0530

    test-run

--- shared/crapi-test/helm_release.tf
+++ shared/crapi-test/helm_release.tf
@@ -1,13 +1,13 @@
-resource "kubernetes_namespace" "example" {
+resource "kubernetes_namespace" "crapi-ns" {
   metadata {
     name = "crapi"
   }
 }
 
 resource "helm_release" "crapi" {
+  depends_on  = [kubernetes_namespace.crapi-ns]
   name       = "crapi"
   chart      = "./helm"
-
   values = [
     file("./helm/values.yaml")
   ]
